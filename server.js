2d8a41e754c38f792212ef1f6e2830af7d2e1e48
unknown <ramidz130@gmail.com>
2021-08-11 21:33:10 +0100

    new libs: - cors, pusher notes: - once db is connected to the server, the server will watch any change in collectiol called messagecontents - when an "insert" change happen server will trigger pusher with "inserted" - pusher will send the inserted message data to messages data which will go the front end

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
 import Pusher from "pusher";
+import cors from "cors";
 
 //app config
 const app = express();
@@ -18,10 +19,11 @@ const pusher = new Pusher({
 
 //middleware
 app.use(express.json());
+app.use(cors());
 
 // DB config
 const connection_url =
-    "mongodb+srv://admin:hsAVCnIy2r1MRFeT@cluster0.db5pt.mongodb.net/whatsapp_db?retryWrites=true&w=majority";
+    "mongodb+srv://admin:YASoNldXC1ZmznVW@cluster0.db5pt.mongodb.net/whatsapp_db?retryWrites=true&w=majority";
 
 mongoose.connect(connection_url, {
     userCreateIndex: true,
@@ -38,7 +40,19 @@ db.once("open", () => {
     const changeStream = msgCollection.watch();
 
     changeStream.on("change", (change) => {
-        console.log(change);
+        console.log("a change occured:", change);
+
+        if (change.operationType === "insert") {
+            const messageDetails = change.fullDocument;
+            pusher.trigger("messages", "inserted", {
+                name: messageDetails.name,
+                message: messageDetails.message,
+                timestamp: messageDetails.timestamp,
+                received: messageDetails.received,
+            });
+        } else {
+            console.log("error triggering pusher");
+        }
     });
 });
 
