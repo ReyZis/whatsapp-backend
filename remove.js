d8af3c68932e46fcac56d55ecf658d920b703f13
unknown <ramidz130@gmail.com>
2021-09-23 16:12:46 +0100

    i added a new file that i can run to delete all the accounts in my mongo database

---
+++ remove.js
@@ -0,0 +1,22 @@
+// this file is used to clean up the data base, run it to empty the data base
+
+import mongoose from "mongoose";
+import Rooms from "./roomsCollection.js";
+import Users from "./userCollection.js";
+
+const connection_url =
+    "mongodb+srv://admin:YASoNldXC1ZmznVW@cluster0.db5pt.mongodb.net/whatsapp_db?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url, {
+    userCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+console.log("deleting the users");
+db.collection("users").deleteMany({});
+console.log("deleteing the rooms");
+db.collection("rooms").deleteMany({});
+console.log("done");
